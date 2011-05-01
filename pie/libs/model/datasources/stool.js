3c5a20b138da87c56af11bff2cfd0e54e3396fc5
Justin Morris <morris.justin@gmail.com>
2011-04-30 22:08:01 -0600

    using JSON.parse for stool

--- pie/libs/model/datasources/stool.js
+++ pie/libs/model/datasources/stool.js
@@ -9,8 +9,10 @@ Stool.prototype.create = function(model, fields, values) {};
 
 Stool.prototype.read = function (model, id) {
 	var self = this;
-	
-	fs.readFile(this.dataLocation + model.tableize() + '/' + id + '.json', 'utf8', function(error, data) { self.emit('read', data); });
+
+	fs.readFile(this.dataLocation + model.tableize() + '/' + id + '.json', 'utf8', function(error, data) {
+		self.emit('read', JSON.parse(data));
+	});
 };
 
 Stool.prototype.update = function (model, id) {};
