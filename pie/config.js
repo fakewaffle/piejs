b2db1045fcd6c994535956e42bf94586462adedf
Justin Morris <morris.justin@gmail.com>
2011-05-07 22:34:02 -0600

    updated config files, dispatcher uses config

--- pie/config.js
+++ pie/config.js
@@ -1,19 +1,20 @@
 exports.config = {
-	"paths" : {
-		"app" : {
-			"path" : __dirname + "/../app",
-			"config" : {
-				"path"     : __dirname + "/../app/config",
-				"core"     : __dirname + "/../app/config/core.js",
-				"database" : __dirname + "/../app/config/database.js"
+	'paths' : {
+		'app' : {
+			'path' : __dirname + '/../app',
+			'config' : {
+				'path'     : __dirname + '/../app/config/',
+				'core'     : __dirname + '/../app/config/core.js',
+				'database' : __dirname + '/../app/config/database.js'
 			},
-			"models" : __dirname + "/../app/models",
-			"views" : {
-				"path"    : __dirname + "/../app/views",
-				"layouts" : __dirname + "/../app/views/layouts",
+			'models' : __dirname + '/../app/models/',
+			'controllers' : __dirname + '/../app/controllers/',
+			'views' : {
+				'path'    : __dirname + '/../app/views/',
+				'layouts' : __dirname + '/../app/views/layouts/',
 			}
 		},
-		"pie" : {
+		'pie' : {
 
 		}
 	}
