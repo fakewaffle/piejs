dd2809f738013bcd2f82ec4569dcd1f814ed3442
Justin Morris <morris.justin@gmail.com>
2011-05-15 21:16:36 -0600

    moved app.js out of pie/ and into piejs/ to better work with nodemon

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require('./libs/inflector').String;
-
-config = require('./config').config;
+config = require('./pie/config').config;
 config.app = {
 	'core'     : require(config.paths.app.config.core).core,
 	'database' : require(config.paths.app.config.database).database
 };
 debug = config.app.core.debug;
+
+require(config.paths.pie.inflector).String;
 
 Model      = require(config.paths.pie.model).Model;
 Controller = require(config.paths.pie.controller).Controller;
