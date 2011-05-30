cf26c736cd59a135b5a5a54f627359be6fe69fdb
Justin Morris <morris.justin@gmail.com>
2011-05-30 07:39:00 -0600

    add "/" for default for folder config

--- app/config/core.default.js
+++ app/config/core.default.js
@@ -1,6 +1,6 @@
 exports.core = {
 	'port'       : 8000,
-	'folder'     : '',
+	'folder'     : '/',
 	'debug'      : true,
 	'secret'     : '1WPFz6pqkQKXk9Vmna69EA5FufcSBKj6iQ3jDxSFy0AyoRSjLeplUIiKULm9h28',
 	'viewEngine' : 'ejs'
