ba6b1857b77cc8af0f3a46aca15cefc6146091d0
Justin Morris <morris.justin@gmail.com>
2011-05-31 23:19:30 -0600

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 pie = {
 	'paths'  : require('./pie/paths').paths,
-	'app'    : {},
+	'app'    : {
+		'models' : {}
+	},
 	'config' : {}
 };
 
@@ -9,24 +11,25 @@ pie.config.app = {
 	'database' : require(pie.paths.app.config.database).database
 };
 
+
 Inflector  = require(pie.paths.pie.inflector);
 Model      = require(pie.paths.pie.model).Model;
 Controller = require(pie.paths.pie.controller).Controller;
 Sanitize   = require(pie.paths.pie.sanitize);
 
-(function() {
-	var fs    = require('fs');
-	var files = fs.readdirSync(pie.paths.app.models);
-	pie.app   = { 'models' : {} };
 
-	files.forEach(function(file) {
-		var modelName = Inflector.capitalize(file.split('.')[0]);
+// Load all the models into pie.app.models
+var fs    = require('fs');
+var files = fs.readdirSync(pie.paths.app.models);
 
-		if (typeof pie.app.models[modelName] === 'undefined' && ! pie.app.models[modelName]) {
-			pie.app.models[modelName] = new Model(require(pie.paths.app.models + modelName.toLowerCase())[modelName]);
-		}
-	});
-})();
+files.forEach(function(file) {
+	var modelName = Inflector.capitalize(file.split('.')[0]);
+
+	if (typeof pie.app.models[modelName] === 'undefined' && ! pie.app.models[modelName]) {
+		pie.app.models[modelName] = new Model(require(pie.paths.app.models + modelName.toLowerCase())[modelName]);
+	}
+});
+
 
 express = require(pie.paths.pie.modules.express);
 server  = express.createServer(
