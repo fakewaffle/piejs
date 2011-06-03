c04162fe6c68ef227d453e3abb3346ae3b4f67f9
Justin Morris <morris.justin@gmail.com>
2011-06-02 20:32:16 -0600

    alphabetized paths for config

--- pie/paths.js
+++ pie/paths.js
@@ -1,25 +1,25 @@
 exports.paths = {
 	'pie' : {
+		'controller' : __dirname + '/libs/controller/controller.js',
+		'datasource' : {
+			'path'  : __dirname + '/libs/model/datasources/',
+			'mysql' : __dirname + '/libs/model/datasources/mysql.js'
+		},
+		'dispatcher' : __dirname + '/dispatcher.js',
+		'faker'      : __dirname + '/node_modules/Faker',
+		'inflector'  : __dirname + '/libs/inflector.js',
+		'model'      : __dirname + '/libs/model/model.js',
 		'modules' : {
 			'path'    : __dirname + '/node_modules/',
 			'express' : __dirname + '/node_modules/express',
 			'mime'    : __dirname + '/node_modules/mime',
 			'mysql'   : __dirname + '/node_modules/mysql'
 		},
-		'dispatcher' : __dirname + '/dispatcher.js',
-		'model'      : __dirname + '/libs/model/model.js',
-		'datasource' : {
-			'path'  : __dirname + '/libs/model/datasources/',
-			'mysql' : __dirname + '/libs/model/datasources/mysql.js'
-		},
-		'controller' : __dirname + '/libs/controller/controller.js',
+		'sanitize'   : __dirname + '/libs/sanitize.js',
 		'view' : {
 			'path'    : __dirname + '/libs/view/',
-			'helpers' : __dirname + '/libs/view/helpers/',
-		},
-		'sanitize'   : __dirname + '/libs/sanitize.js',
-		'inflector'  : __dirname + '/libs/inflector.js',
-		'faker'      : __dirname + '/node_modules/Faker'
+			'helpers' : __dirname + '/libs/view/helpers/'
+		}
 	},
 	'app' : {
 		'path' : __dirname + '/../app' + '/',
@@ -28,19 +28,19 @@ exports.paths = {
 			'core'     : __dirname + '/../app/config/core.js',
 			'database' : __dirname + '/../app/config/database.js'
 		},
-		'models' : __dirname + '/../app/models/',
 		'controllers' : __dirname + '/../app/controllers/',
-		'views' : {
-			'path'    : __dirname + '/../app/views',
-			'layouts' : __dirname + '/../app/views/layouts/',
-			'pages'   : __dirname + '/../app/views/pages',
-			'helpers' : __dirname + '/../app/views/helpers/'
-		},
+		'models' : __dirname + '/../app/models/',
 		'public' : {
 			'path'        : __dirname + '/../app/public/',
 			'images'      : __dirname + '/../app/public/images/',
 			'javascripts' : __dirname + '/../app/public/javascripts/',
 			'stylesheets' : __dirname + '/../app/public/stylesheets/'
+		},
+		'views' : {
+			'path'    : __dirname + '/../app/views',
+			'helpers' : __dirname + '/../app/views/helpers/',
+			'layouts' : __dirname + '/../app/views/layouts/',
+			'pages'   : __dirname + '/../app/views/pages'
 		}
 	}
 };
