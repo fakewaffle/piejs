0e58b95133b29dc573c7d25c36427956ae82972e
Justin Morris <morris.justin@gmail.com>
2011-06-21 23:04:02 -0600

    fixed a bug i ran into in inflector caused by code formating

--- pie/libs/inflector.js
+++ pie/libs/inflector.js
@@ -95,56 +95,56 @@ var uncountableWords = [
 
 // These rules translate from the singular form of a noun to its plural form.
 var pluralRules = [
-	[ new RegExp('(m)an$', 'gi'), '$1en' ],
-	[ new RegExp('(pe)rson$', 'gi'), '$1ople' ],
-	[ new RegExp('(child)$', 'gi'), '$1ren' ],
-	[ new RegExp('^(ox)$', 'gi'), '$1en' ],
-	[ new RegExp('(ax|test)is$', 'gi'), '$1es' ],
-	[ new RegExp('(octop|vir)us$', 'gi'), '$1i' ],
-	[ new RegExp('(alias|status)$', 'gi'), '$1es' ],
-	[ new RegExp('(bu)s$', 'gi'), '$1ses' ],
+	[ new RegExp('(m)an$', 'gi'),                 '$1en' ],
+	[ new RegExp('(pe)rson$', 'gi'),              '$1ople' ],
+	[ new RegExp('(child)$', 'gi'),               '$1ren' ],
+	[ new RegExp('^(ox)$', 'gi'),                 '$1en' ],
+	[ new RegExp('(ax|test)is$', 'gi'),           '$1es' ],
+	[ new RegExp('(octop|vir)us$', 'gi'),         '$1i' ],
+	[ new RegExp('(alias|status)$', 'gi'),        '$1es' ],
+	[ new RegExp('(bu)s$', 'gi'),                 '$1ses' ],
 	[ new RegExp('(buffal|tomat|potat)o$', 'gi'), '$1oes' ],
-	[ new RegExp('([ ti ])um$', 'gi'), '$1a' ],
-	[ new RegExp('sis$', 'gi'), 'ses' ],
-	[ new RegExp('(?:([ ^f ])fe|([ lr ])f)$', 'gi'), '$1$2ves' ],
-	[ new RegExp('(hive)$', 'gi'), '$1s' ],
-	[ new RegExp('([ ^aeiouy ]|qu)y$', 'gi'), '$1ies' ],
-	[ new RegExp('(x|ch|ss|sh)$', 'gi'), '$1es' ],
-	[ new RegExp('(matr|vert|ind)ix|ex$', 'gi'), '$1ices' ],
-	[ new RegExp('([ m|l ])ouse$', 'gi'), '$1ice' ],
-	[ new RegExp('(quiz)$', 'gi'), '$1zes' ],
-	[ new RegExp('s$', 'gi'), 's' ],
-	[ new RegExp('$', 'gi'), 's' ]
+	[ new RegExp('([ti])um$', 'gi'),              '$1a' ],
+	[ new RegExp('sis$', 'gi'),                   'ses' ],
+	[ new RegExp('(?:([^f])fe|([lr])f)$', 'gi'),  '$1$2ves' ],
+	[ new RegExp('(hive)$', 'gi'),                '$1s' ],
+	[ new RegExp('([^aeiouy]|qu)y$', 'gi'),       '$1ies' ],
+	[ new RegExp('(x|ch|ss|sh)$', 'gi'),          '$1es' ],
+	[ new RegExp('(matr|vert|ind)ix|ex$', 'gi'),  '$1ices' ],
+	[ new RegExp('([m|l])ouse$', 'gi'),           '$1ice' ],
+	[ new RegExp('(quiz)$', 'gi'),                '$1zes' ],
+	[ new RegExp('s$', 'gi'),                     's' ],
+	[ new RegExp('$', 'gi'),                      's' ]
 ];
 
 // These rules translate from the plural form of a noun to its singular form.
 var singularRules = [
-	[ new RegExp('(m)en$', 'gi'), '$1an' ],
-	[ new RegExp('(pe)ople$', 'gi'), '$1rson' ],
-	[ new RegExp('(child)ren$', 'gi'), '$1' ],
-	[ new RegExp('([ ti ])a$', 'gi'), '$1um' ],
+	[ new RegExp('(m)en$', 'gi'),                                                       '$1an' ],
+	[ new RegExp('(pe)ople$', 'gi'),                                                    '$1rson' ],
+	[ new RegExp('(child)ren$', 'gi'),                                                  '$1' ],
+	[ new RegExp('([ti])a$', 'gi'),                                                     '$1um' ],
 	[ new RegExp('((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$','gi'), '$1$2sis' ],
-	[ new RegExp('(hive)s$', 'gi'), '$1' ],
-	[ new RegExp('(tive)s$', 'gi'), '$1' ],
-	[ new RegExp('(curve)s$', 'gi'), '$1' ],
-	[ new RegExp('([ lr ])ves$', 'gi'), '$1f' ],
-	[ new RegExp('([ ^fo ])ves$', 'gi'), '$1fe' ],
-	[ new RegExp('([ ^aeiouy ]|qu)ies$', 'gi'), '$1y' ],
-	[ new RegExp('(s)eries$', 'gi'), '$1eries' ],
-	[ new RegExp('(m)ovies$', 'gi'), '$1ovie' ],
-	[ new RegExp('(x|ch|ss|sh)es$', 'gi'), '$1' ],
-	[ new RegExp('([ m|l ])ice$', 'gi'), '$1ouse' ],
-	[ new RegExp('(bus)es$', 'gi'), '$1' ],
-	[ new RegExp('(o)es$', 'gi'), '$1' ],
-	[ new RegExp('(shoe)s$', 'gi'), '$1' ],
-	[ new RegExp('(cris|ax|test)es$', 'gi'), '$1is' ],
-	[ new RegExp('(octop|vir)i$', 'gi'), '$1us' ],
-	[ new RegExp('(alias|status)es$', 'gi'), '$1' ],
-	[ new RegExp('^(ox)en', 'gi'), '$1' ],
-	[ new RegExp('(vert|ind)ices$', 'gi'), '$1ex' ],
-	[ new RegExp('(matr)ices$', 'gi'), '$1ix' ],
-	[ new RegExp('(quiz)zes$', 'gi'), '$1' ],
-	[ new RegExp('s$', 'gi'), '' ]
+	[ new RegExp('(hive)s$', 'gi'),                                                     '$1' ],
+	[ new RegExp('(tive)s$', 'gi'),                                                     '$1' ],
+	[ new RegExp('(curve)s$', 'gi'),                                                    '$1' ],
+	[ new RegExp('([lr])ves$', 'gi'),                                                   '$1f' ],
+	[ new RegExp('([^fo])ves$', 'gi'),                                                  '$1fe' ],
+	[ new RegExp('([^aeiouy]|qu)ies$', 'gi'),                                           '$1y' ],
+	[ new RegExp('(s)eries$', 'gi'),                                                    '$1eries' ],
+	[ new RegExp('(m)ovies$', 'gi'),                                                    '$1ovie' ],
+	[ new RegExp('(x|ch|ss|sh)es$', 'gi'),                                              '$1' ],
+	[ new RegExp('([m|l])ice$', 'gi'),                                                  '$1ouse' ],
+	[ new RegExp('(bus)es$', 'gi'),                                                     '$1'],
+	[ new RegExp('(o)es$', 'gi'),                                                       '$1' ],
+	[ new RegExp('(shoe)s$', 'gi'),                                                     '$1' ],
+	[ new RegExp('(cris|ax|test)es$', 'gi'),                                            '$1is' ],
+	[ new RegExp('(octop|vir)i$', 'gi'),                                                '$1us' ],
+	[ new RegExp('(alias|status)es$', 'gi'),                                            '$1' ],
+	[ new RegExp('^(ox)en', 'gi'),                                                      '$1' ],
+	[ new RegExp('(vert|ind)ices$', 'gi'),                                              '$1ex' ],
+	[ new RegExp('(matr)ices$', 'gi'),                                                  '$1ix' ],
+	[ new RegExp('(quiz)zes$', 'gi'),                                                   '$1' ],
+	[ new RegExp('s$', 'gi'),                                                           '' ]
 ];
 
 // This is a list of words that should not be capitalized for title case
@@ -195,42 +195,6 @@ applyRules = function(string, rules, skip, override) {
 	}
 
 	return string;
-}
-
-/**
- * This lets us detect if an Array contains a given element
- *
- * Arguments:
- * 		item - Object - object to locate in the Array
- * 		fromIndex - Integer (optional) - starts checking from this position in the Array
- * 		compareFunc - Function (optional) - function used to compare Array item vs passed item
- *
- * Returns:
- * 		Integer - index position in the Array of the passed item
- *
- * Examples:
- * 		['hi','there'].indexOf("guys") === -1
- * 		['hi','there'].indexOf("hi") === 0
- *
- * 2011-05-30 12.23.30 - Updated by Justin Morris
- */
-if (typeof Array.prototype.indexOf === 'undefined' && !Array.prototype.indexOf) {
-	Array.prototype.indexOf = function(item, fromIndex, compareFunc) {
-		if (!fromIndex) {
-			fromIndex = -1;
-		}
-
-		var index = -1;
-
-		for (var i = fromIndex; i < this.length; i++) {
-			if (this[i] === item || compareFunc && compareFunc(this[i], item)) {
-				index = i;
-				break;
-			}
-		}
-
-		return index;
-	}
 }
 
 /**
